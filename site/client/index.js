813657dabad76250a422da03282e9e9f95c3d020
Ryan James <rcjames1004@gmail.com>
2016-02-06 15:06:08 -0800

    Attach handlers to menu

--- site/client/index.js
+++ site/client/index.js
@@ -1,11 +1,8 @@
 if(Meteor.isClient) {
 	Template.index.onRendered(function () {
-		$("#hello").click( function () {
-			console.log("Action triggered");
-			var rotation = $(".heart").get(0).getAttribute("rotation");
-			rotation.y += 30;
-			$(".heart").get(0).setAttribute("rotation", String(rotation.x) + " " + String(rotation.y) +
-				" " + String(rotation.z));
+		$(".menuItem").click( function () {
+			var organ = $( this ).attr("data-organ");
+			alert("'" + organ + "' clicked!");
 		});
 	});
 }
