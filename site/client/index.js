31f02b7acbfc0f7256431785b02e5fe5b0ea239c
Ac2zoom <ac2zoom@yahoo.com>
2016-02-06 14:50:51 -0800

    Added new functions for arrows

--- site/client/index.js
+++ site/client/index.js
@@ -1,7 +1,13 @@
 if(Meteor.isClient) {
 	Template.index.onRendered(function () {
-		$("#hello").click( function () {
-			console.log("Action triggered");
+		$("#leftarrow").click( function () {
+			var rotation = $(".heart").get(0).getAttribute("rotation");
+			rotation.y -= 30;
+			$(".heart").get(0).setAttribute("rotation", String(rotation.x) + " " + String(rotation.y) +
+				" " + String(rotation.z));
+		});
+
+		$("#rightarrow").click( function () {
 			var rotation = $(".heart").get(0).getAttribute("rotation");
 			rotation.y += 30;
 			$(".heart").get(0).setAttribute("rotation", String(rotation.x) + " " + String(rotation.y) +
