e2cd946a1e548bd8f77bc50d5e8ed426e9c05c43
Ac2zoom <ac2zoom@yahoo.com>
2016-02-06 15:17:19 -0800

    Added interactive loading of lungs

--- site/client/index.js
+++ site/client/index.js
@@ -13,12 +13,9 @@ if(Meteor.isClient) {
 			$(".organ").get(0).setAttribute("rotation", String(rotation.x) + " " + String(rotation.y) +
 				" " + String(rotation.z));
 		});
-		$("#leftarrow").click( function () {
-			console.log("Action triggered");
-			var rotation = $(".organ").get(0).getAttribute("rotation");
-			rotation.y -= 30;
-			$(".organ").get(0).setAttribute("rotation", String(rotation.x) + " " + String(rotation.y) +
-				" " + String(rotation.z));
+
+		$("#lungsButton").click( function () {
+			$(".organ").get(0).setAttribute("src", '/models/lungs/Lungs.dae');
 		});
 	});
 }
