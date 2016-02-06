4bf5fe51ced8f9a92cbdaefab52d90107d1e8eff
Calob Mejias <calob@me.com>
2016-02-06 15:26:55 -0800

    changes

--- site/client/index.js
+++ site/client/index.js
@@ -1,14 +1,14 @@
 if(Meteor.isClient) {
 	Template.index.onRendered(function () {
 		$("#leftarrow").click( function () {
-			var rotation = $(".heart").get(0).getAttribute("rotation");
+			var rotation = $(".organ").get(0).getAttribute("rotation");
 			rotation.y -= 30;
-			$(".heart").get(0).setAttribute("rotation", String(rotation.x) + " " + String(rotation.y) +
+			$(".organ").get(0).setAttribute("rotation", String(rotation.x) + " " + String(rotation.y) +
 				" " + String(rotation.z));
 		});
 
 		$("#rightarrow").click( function () {
-			var rotation = $(".heart").get(0).getAttribute("rotation");
+			var rotation = $(".organ").get(0).getAttribute("rotation");
 			rotation.y += 30;
 			$(".organ").get(0).setAttribute("rotation", String(rotation.x) + " " + String(rotation.y) +
 				" " + String(rotation.z));
