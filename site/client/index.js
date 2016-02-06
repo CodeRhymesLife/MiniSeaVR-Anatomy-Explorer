9d03664330b6239b767237a90d7ea89f245ac2b4
Ac2zoom <ac2zoom@yahoo.com>
2016-02-06 12:57:21 -0800

    Added Heart model

--- site/client/index.js
+++ site/client/index.js
@@ -1,12 +1,8 @@
 if(Meteor.isClient) {
 	Template.index.onRendered(function () {
 		$(".menu .item").click(function () {
-			$( this ).get(0).setAttribute("color", "black");
-			$( this ).get(0).emit('attrchanged', {
-				name: "color",
-				newData: "black",
-				oldData: "yellow",
-			});
+			var rotation = $( this ).get(0).getAttribute("rotation");
+			$( this ).get(0).setAttribute("rotation", "");
 		});
 	});
 }
