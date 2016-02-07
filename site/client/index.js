26226a8e4256d99f2b3e7e06537299e885cf0ca9
Ryan James <rcjames1004@gmail.com>
2016-02-06 17:33:54 -0800

    Update sounds

--- site/client/index.js
+++ site/client/index.js
@@ -7,20 +7,14 @@ if(Meteor.isClient) {
 				" " + String(rotation.z));
 		});
 
-		var organToSound = {
-			"Brain": "#HighPitchMeduClick",
-			"Lungs": "#HighPitchMeduClick",
-			"Heart": "#MediumPitchMenuClick",
-			"Stomach": "#LowPitchMenuClick",
-			"Liver": "#LowPitchMenuClick",
-		};
+		var menuClickSound = $( "#MenuClickSound" ).get(0).components.sound;
 		$(".menuItem").click( function () {
 			var organ = $( this ).attr("data-organ");
 			$(".organ").get(0).setAttribute("src", '/models/organs/' + organ + '.dae');
 			$("#fullbody").get(0).setAttribute("src", '/images/' + organ + '.jpg');
 			
 			// Play sound when menu button clicked
-			$( organToSound[organ] ).get(0).components.sound.play();
+			menuClickSound.play();
 		});
 
 		$("#rightarrow").click( function () {
