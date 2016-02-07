11d1de8388b5db756bedd3bd9968fc0e003dcb88
Calob Mejias <calob@me.com>
2016-02-06 16:02:14 -0800

    Images functionality added

--- site/client/index.js
+++ site/client/index.js
@@ -7,10 +7,11 @@ if(Meteor.isClient) {
 			$(".organ").get(0).setAttribute("rotation", String(rotation.x) + " " + String(rotation.y) +
 				" " + String(rotation.z));
 		});
-		
+
 		$(".menuItem").click( function () {
 			var organ = $( this ).attr("data-organ");
 			$(".organ").get(0).setAttribute("src", '/models/organs/' + organ + '.dae');
+			$("#fullbody").get(0).setAttribute("src", '/images/' + organ + '.jpg');
 		});
 
 		$("#rightarrow").click( function () {
